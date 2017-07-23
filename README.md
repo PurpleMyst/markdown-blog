897a6efe2365d9b8bae0faed73bd9e15aafe9ffe
Purple Myst <PurpleMyst@users.noreply.github.com>
2017-07-23 22:25:04 +0200

    Add HTML template.

---
+++ README.md
@@ -0,0 +1,16 @@
+# markdown-blog
+
+`markdown-blog` is a simple application written in Node.js, utilizing
+[Express.js](http://expressjs.com/) and [markdown-js](https://github.com/evilstreak/markdown-js) for having simple blogs using Markdown.
+
+
+# Usage
+
+1. Customize `template.html` to your liking, including CSS styles, headers,
+   footers, scripts, etcetera...
+2. Place your markdown documents in the `pages/` folder. These will be
+   accessible by the index, or by going to the page directly.
+3. Run `node index.js`.
+4. ???
+5. Success! You will now be able to access your little blog on `localhost:8080`
+   with default settings.
--- index.js
+++ index.js
@@ -8,33 +8,33 @@ const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 8080;
 
-let pageStyle;
+let pageTemplate;
 
 app.get("/", (req, res) => {
+  console.info("Got asked for index.");
   res.send("No index right now.");
 });
 
-app.get("/page/:page", (req, res) => {
-  fs.readFile(`./pages/${req.params.page}`, "utf8", (err, data) => {
+app.get("/:page", (req, res) => {
+  const page = req.params.page;
+
+  console.info(`Got asked to serve ${page}.`);
+  fs.readFile(`./pages/${page}`, "utf8", (err, data) => {
     if (err === null) {
-      let responseHTML = "";
-
-      /* Add styling info */
-      responseHTML += `<style>${pageStyle}</style>`
-
-      /* Render the markdown */
-      responseHTML += markdown.toHTML(data);
-
-      res.send(responseHTML);
+      console.info("Found page!");
+      res.send(pageTemplate.replace(/__MARKDOWN__/g, markdown.toHTML(data)));
     } else {
+      console.error("Couldn't find page.");
       res.status(404)
-         .send(`No page named ${req.params.page}.`);
+         .send(`No page named ${page}.`);
     }
   });
 });
 
-fs.readFile("./style.css", "utf8", (err, data) => {
+fs.readFile("./template.html", "utf8", (err, data) => {
   if (err) throw err;
-  pageStyle = data;
+
+  console.info("Starting.");
+  pageTemplate = data;
   app.listen(port);
 });
